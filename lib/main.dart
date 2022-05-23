c5c08deacb52fbb0267cdd3f785053922a37b937
martinpelli <martinpelli@outlook.com>
2022-05-23 14:09:38 -0300

    first structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_preferences_app/screens/screens.dart';
+import 'package:flutter_preferences_app/screens/settings_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Preferences App',
+      initialRoute: HomeScreen.routerName,
+      routes: {
+        HomeScreen.routerName: (_) => const HomeScreen(),
+        SettingsScreen.routerName: (_) => const SettingsScreen()
+      },
     );
   }
 }
---
+++ lib/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:flutter_preferences_app/screens/home_screen.dart';
+export 'package:flutter_preferences_app/screens/settings_screen.dart';
